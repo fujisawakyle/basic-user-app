70d60e1725f42d7d3c7a550646ed1c2e8a14d0d7
fujisawakyle <fujisawakyle@gmail.com>
2017-09-04 08:28:40 -0700

    basic login incomplete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def message
-    render html: "This will be a basic login app"
-  end
+  include SessionsHelper
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to user_url(@user)
     else
       render 'new'
     end
